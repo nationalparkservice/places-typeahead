b54f40ba648c11dafd6e855b2e1aab0322f522e1
Jim McAndrew <jim@loc8.us>
2015-08-03 13:52:25 -0600

    Add the upload and messaging tools

--- examples/submit/assets/js/main.js
+++ examples/submit/assets/js/main.js
@@ -2,11 +2,11 @@
 
 /*
 
-  - Check against InsideMaps status to see if user is logged in.
-  - If they are, show map, etc.
-  - If they aren't, redirect them to the InsideMaps login page
-    - Pass hash information in so it is captured properly
-    - Once they've saved a point, call the function, via postMessage, that is passed in via the URL
+   - Check against InsideMaps status to see if user is logged in.
+   - If they are, show map, etc.
+   - If they aren't, redirect them to the InsideMaps login page
+   - Pass hash information in so it is captured properly
+   - Once they've saved a point, call the function, via postMessage, that is passed in via the URL
 
 */
 
@@ -19,13 +19,14 @@ $(document).ready(function () {
   var auth = osmAuth({
     oauth_consumer_key: 'F7zPYlVCqE2BUH9Hr4SsWZSOnrKjpug1EgqkbsSb',
     oauth_secret: 'rIkjpPcBNkMQxrqzcOvOC4RRuYupYr7k8mfP13H5',
-    url: 'http://10.147.153.193'
+    url: 'http://10.147.153.193',
+    modal: true
   });
-  var s = document.createElement('script');
   var $name;
   var $type;
   var marker;
   var name;
+  var s = document.createElement('script');
   var type;
   var types;
 
@@ -41,6 +42,7 @@ $(document).ready(function () {
     $name.focus();
     $type.val(type);
   }
+
   function generateForm () {
     var interval = setInterval(function () {
       $name = $('#name');
@@ -65,7 +67,8 @@ $(document).ready(function () {
           geojson.properties.name = name;
           geojson.properties['nps:preset'] = type;
           console.log(geojson);
-          console.log(geojson2osm(geojson, null, iD.data.presets.presets));
+          // console.log(geojson2osm(geojson, null, iD.data.presets.presets));
+          uploadGeojson(geojson);
           return false;
         });
 
@@ -84,7 +87,7 @@ $(document).ready(function () {
 
   auth.xhr({
     method: 'GET',
-    path: 'http://10.147.153.193/api/0.6/user/details'
+    path: '/api/0.6/user/details'
   }, function (error, response) {
     console.log(error);
     console.log(response);
@@ -98,6 +101,51 @@ $(document).ready(function () {
       // Show the map.
     }
   });
+
+  var uploadGeojson = function (geojson) {
+    var changeset = {};
+
+    // Create the Changeset
+    auth.xhr({
+      'method': 'PUT',
+      'path': '/api/0.6/changeset/create',
+      'content': '<osm><changeset version="0.3" generator="npmap-uploader"><tag k="created_by" v="npmap-uploader"/><tag k="locale" v="en-US"/><tag k="comment" v="Parking"/></changeset></osm>',
+      options: {
+        header: {
+          'Content-Type': 'text/xml'
+        }
+      }
+    }, function (err, result) {
+      if (!err && result) {
+        changeset = {
+          'data': geojson2osm(geojson, result, iD.data.presets.presets),
+          'id': result
+        };
+
+        // Update the changeset
+        auth.xhr({
+          'method': 'POST',
+          'path': '/api/0.6/changeset/' + changeset.id + '/upload',
+          'content': changeset.data,
+          options: {
+            header: {
+              'Content-Type': 'text/xml'
+            }
+          }
+        }, function (e, r) {
+          console.log('e', e, 'r', r);
+
+          // Close the changeset
+          auth.xhr({
+            'method': 'PUT',
+            'path': '/api/0.6/changeset/' + changeset.id + '/close'
+          }, function (e, r) {
+            console.log('e', e, 'r', r);
+          });
+        });
+      }
+    });
+  };
 
   NPMap = {
     baseLayers: [
@@ -117,20 +165,20 @@ $(document).ready(function () {
           })
             .setContent('' +
               '<form>' +
-                '<div class="form-group">' +
-                  '<label for="name">Name</label>' +
-                  '<input class="form-control" id="name" type="text">' +
-                '</div>' +
-                '<div class="form-group">' +
-                  '<label for="type=">Type</label>' +
-                  '<select class="form-control" id="type">' +
-                  '</select>' +
-                '</div>' +
-                '<div style="text-align:center;">' +
-                  '<button class="btn btn-primary">Submit</button>' +
-                '</div>' +
+              '<div class="form-group">' +
+              '<label for="name">Name</label>' +
+              '<input class="form-control" id="name" type="text">' +
+              '</div>' +
+              '<div class="form-group">' +
+              '<label for="type=">Type</label>' +
+              '<select class="form-control" id="type">' +
+              '</select>' +
+              '</div>' +
+              '<div style="text-align:center;">' +
+              '<button class="btn btn-primary">Submit</button>' +
+              '</div>' +
               '</form>' +
-            '');
+              '');
 
           document.getElementsByClassName('panel-body')[0].childNodes[0].innerHTML = 'Drag the marker to position it then fill out and submit the form.';
           marker = L.marker(e.latlng, {
@@ -154,11 +202,11 @@ $(document).ready(function () {
         div.className = 'wrapper';
         div.innerHTML = '' +
           '<div class="panel panel-default">' +
-            '<div class="panel-body">' +
-              '<p>Click the map to add a location.</p>' +
-            '</div>' +
+          '<div class="panel-body">' +
+          '<p>Click the map to add a location.</p>' +
           '</div>' +
-        '';
+          '</div>' +
+          '';
         document.getElementsByClassName('npmap-container')[0].appendChild(div);
         L.npmap.util._.reqwest({
           success: function (response) {
@@ -188,4 +236,17 @@ $(document).ready(function () {
   };
   s.src = 'http://www.nps.gov/lib/npmap.js/2.0.0/npmap-bootstrap.min.js';
   document.body.appendChild(s);
+
+  window.addEventListener('message', receiveMessage, false);
+
+  function receiveMessage (event) {
+    if (event.origin !== 'http://insidemaps.nps.gov') {
+      return;
+    } else {
+      console.log(event);
+      authComplete(event.data);
+    }
+  }
+
 });
+
